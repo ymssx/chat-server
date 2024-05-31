7a0c72c0ffca638d477542eb6483ba7d52c56c39
yami <yamiiyang@tencent.com>
2024-06-01 00:24:22 +0800

    chore: 调整路由

--- app/router.js
+++ app/router.js
@@ -5,7 +5,7 @@
  */
 module.exports = app => {
   const { router, controller } = app;
-  router.get('/', controller.home.index);
+  router.get('/api', controller.home.index);
   router.get('/api/chat/sessions/', controller.chat.getSessions);
   router.post('/api/message/send/', controller.chat.sendMessage);
 };
